e37055a4031da5f43c98858813441e18bde57f1a
issueye <issueye@yeah.net>
2024-10-15 19:16:01 +0800

    新加服务查询页面

---
+++ pages/server_query/Frm_server_qryImpl.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "github.com/ying32/govcl/vcl"
+)
+
+//::private::
+type TFrm_server_qryFields struct {
+}
+
+func (f *TFrm_server_qry) OnFormCreate(sender vcl.IObject) {
+
+}
---
+++ pages/server_query/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "github.com/ying32/govcl/vcl"
+    _ "github.com/ying32/govcl/pkgs/winappres"
+)
+
+func main() {
+    vcl.RunApp(&Frm_server_qry)
+}
